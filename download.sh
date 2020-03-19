b137ffbc51b9ac64bc535c7849a6eee48f583a41
fanlia <fanlia@outlook.com>
2020-03-19 08:53:29 +0800

    refine download.sh

--- download.sh
+++ download.sh
@@ -3,8 +3,8 @@ data_link=https://file.hankcs.com/hanlp/data-for-1.7.5.zip
 ChnSentiCorp_link=https://file.hankcs.com/corpus/ChnSentiCorp.zip
 
 download() {
-    wget -P static/ $1
-    unzip -o -d static/ static/`basename $1`
+    wget -c -P static/ $1
+    unzip -q -o -d static/ static/`basename $1`
 }
 
 echo "下载 hanlp data"
